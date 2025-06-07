f78329083b09e56c6eeb379bc5fd162b2db0874d
not-dyake <dyakesalalila2504@gmail.com>
2025-06-07 22:06:37 +0800

    Normalize line endings with .gitattributes

--- student_management/urls.py
+++ student_management/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from sms_app import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('', include('sms_app.urls')),
-    path('/', views.home, name='home'),
+    
 ]
