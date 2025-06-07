3edd48ed266d2cdfd2f0cea8dd7ad9410398cfc8
not-dyake <dyakesalalila2504@gmail.com>
2025-06-07 21:33:45 +0800

    Normalize line endings with .gitattributes

--- student_management/settings.py
+++ student_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-59jo7jtr4%zh8an_3#$tbgi4np_%6*t+euk2c4s+d^@-#y7n(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stud_managemeny_system_admin.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['students-management-system-admin.onrender.com/', 'localhost']
 
 
 # Application definition
@@ -118,4 +118,4 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
