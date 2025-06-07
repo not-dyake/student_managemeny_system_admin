75ec71fde9350a383d5255007692cba7d8560edc
not-dyake <dyakesalalila2504@gmail.com>
2025-06-08 00:56:50 +0800

    Normalize line endings with .gitattributes

--- student_management/settings.py
+++ student_management/settings.py
@@ -49,10 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'student_management.urls'
 
